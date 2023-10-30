b2018b69545968f1ccd94348bb4540d1f73ecee1
LaSnipeuse <floriane.blanchard.pro@gmail.com>
2023-10-30 21:06:59 +0100

    Add gossip and user controller with their show so now I have gossip.all on my home, then one specific page per gossip and per author

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
-  get 'static_pages/team'
-  get 'static_pages/contact'
-  
-  get "up" => "rails/health#show"
-
+  root to: "gossips#index" #donne l'info sur où se trouve la racine
+  get 'team', to: 'static_pages#team'
+  get 'contact', to: 'static_pages#contact'
+  get 'gossip/:id', to: 'gossips#show', as: 'gossip'
+  get 'author/:id', to: 'users#show', as: 'user'
 end
